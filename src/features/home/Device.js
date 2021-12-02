803e3b337539d5c09aa0748ad03201efd8c8c331
Dzung DH <dzung.dh3510@gmail.com>
2021-12-03 01:22:45 +0700

    Add screen home and its related fragments: location and device

---
+++ src/features/home/Device.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class Device extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Device Fragment</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export { Device as DeviceFragment };
