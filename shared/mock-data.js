e234cd64ee2c2f87fbfbb084cf944305c3a0bf90
Dzung DH <dzung.dh3510@gmail.com>
2021-12-07 22:20:20 +0700

    Add mock data

---
+++ shared/mock-data.js
@@ -0,0 +1,80 @@
+const airQuality = {
+   "Good": {
+      pollutants: {
+         so2: 10,
+         co: 20,
+         no2: 30,
+         o3: 40,
+         pm2_5: 20,
+         pm10: 100
+      },
+      aqi: 30,
+      location: "Hà Nội",
+      time: null
+   },
+   "Moderate": {
+      pollutants: {
+         so2: 10,
+         co: 20,
+         no2: 30,
+         o3: 40,
+         pm2_5: 20,
+         pm10: 100
+      },
+      aqi: 100,
+      location: "Hà Nội",
+      time: null
+   },
+   "Unhealthy1": {
+      pollutants: {
+         so2: 10,
+         co: 20,
+         no2: 30,
+         o3: 40,
+         pm2_5: 20,
+         pm10: 100
+      },
+      aqi: 150,
+      location: "Hà Nội",
+      time: null
+   },
+   "Unhealthy2": {
+      pollutants: {
+         so2: 10,
+         co: 20,
+         no2: 30,
+         o3: 40,
+         pm2_5: 20,
+         pm10: 100
+      },
+      aqi: 250,
+      location: "Hà Nội",
+      time: null
+   },
+   "Unhealthy3": {
+      pollutants: {
+         so2: 10,
+         co: 20,
+         no2: 30,
+         o3: 40,
+         pm2_5: 20,
+         pm10: 100
+      },
+      aqi: 320,
+      location: "Hà Nội",
+      time: null
+   },
+   "Hazardous": {
+      pollutants: {
+         so2: 10,
+         co: 20,
+         no2: 30,
+         o3: 40,
+         pm2_5: 20,
+         pm10: 100
+      },
+      aqi: 500,
+      location: "Hà Nội",
+      time: null
+   }
+};
