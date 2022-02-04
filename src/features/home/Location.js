9f07067b53e4377aa3302f0ebb1dde45bfc5beb6
Dzung DH <dzung.dh3510@gmail.com>
2022-02-04 10:12:09 +0700

    Add mqtt lib and demo code

--- src/features/home/Location.js
+++ src/features/home/Location.js
@@ -2,6 +2,41 @@ import React from 'react';
 import { StyleSheet, View, Text, FlatList } from 'react-native';
 import { Card } from '@components/card/Card';
 
+import MQTT from 'sp-react-native-mqtt';
+
+console.log('test')
+// uri: "mqtt://18.193.126.219:1883",
+let options = {
+  uri: "mqtt://broker.hivemq.com:1883 ",
+  user: "",
+  pass: "",
+  auth: true,
+  keepalive: 32000
+  }
+  options.clientId = "clientId"+Date.now()
+  MQTT.createClient(options).then((client) => {
+    client.connect();
+    client.on('closed', () => {
+      console.log('mqtt.event.closed');
+    });
+  
+    client.on('error', (msg) => {
+      console.log('bin mqtt.event.error', msg);
+    });
+  
+    client.on('message', (msg) => {
+      console.log('mqtt.event.message', msg);
+    });
+  
+    client.on('connect', () => {
+      console.log('connected');
+      client.subscribe('topic_name', 2)
+    });
+  
+  }).catch((err) => {
+    console.log("err : " + err);
+  });
+
 const styles = StyleSheet.create({
     list: {
         paddingHorizontal: 14,
